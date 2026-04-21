62196f9a559be0e31449f2303b69c60072676561
Saimou Zhang <zhangsaimou@yahoo.fr>
2026-04-21 01:12:52 -0500

    remove duplicate CORS headers

--- gpt-writing-backend/server.py
+++ gpt-writing-backend/server.py
@@ -8,12 +8,6 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.after_request
-def after_request(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
-    response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
-    return response
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 model_type = "deepseek-chat"
